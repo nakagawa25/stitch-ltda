ccd9779ce593728845cd0296c85cff4fbe65c286
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2023-12-31 13:56:58 -0300

    Add project files.

---
+++ StitchLtda/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StitchLtda.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+                _configuration = configuration;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return Ok("Stitch LTDA rodando com exito. -> Ambiente: " + _configuration["Calopsita"]);
+        }
+    }
+}
